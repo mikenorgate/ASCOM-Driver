768e7e7e209a39fabe88b68d223ecc9cabaa5fae
Mike Norgate <github@mike-norgate.com>
2015-08-27 16:54:44 +0100

    Initial Commit of driver template

---
+++ Norgate.ASCOM.Driver.UnitTests/DriverTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Norgate.ASCOM.Driver.UnitTests
+{
+    [TestClass]
+    public class DriverTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
